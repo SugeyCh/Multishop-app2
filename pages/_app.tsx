2001411e40055ef828e9406cbabc4a5b077f7768
SugeyCh <chaconsugey2003@gmail.com>
2024-05-20 18:08:36 -0400

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Multishop KPI</title>
+        <link rel="manifest" href="manifest.json" />
+        <link rel="icon" href="/icon512_maskable.png" />
+        <link rel="apple-touch-icon" href="/icon512_rounded.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
